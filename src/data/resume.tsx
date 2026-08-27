6c16e42aa7a625fa058968aef8c1d4fc35432db4
ali-sorathiya64 <alisorathiya970@gmail.com>
2026-08-27 01:46:17 -0700

    edit some rename changes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -96,7 +96,7 @@ export const DATA: DataType = {
       items: ["Git", "GitHub"],
     },
   ],
-  resumeUrl: "/sa_resume.pdf",
+  resumeUrl: "/sa-resume.pdf",
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
