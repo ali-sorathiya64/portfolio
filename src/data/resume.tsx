25e3006081c5efe9bad5143715c1c927fd62c319
Ali sorathiya <alisorathiya970@gmail.com>
2026-09-03 12:03:51 -0700

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -241,7 +241,7 @@ avatarUrl: "/mine2.jpg",
       links: [
         {
           type: "Source",
-          href: "https://github.com/ali-sorathiya64/",
+          href: "https://github.com/ali-sorathiya64/ai-resume-reviewer/",
           icon: <Icons.github className="size-3" />,
         },
       ],
