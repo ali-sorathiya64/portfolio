13e234e25a0cf671a588233cab93713cb98b98d0
Ali sorathiya <alisorathiya970@gmail.com>
2026-09-16 11:18:33 -0700

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,7 +11,7 @@ export function formatDate(date: string) {
 
   // Check if targetDate is a valid date
   if (isNaN(targetDate.getTime())) {
-    return ""; // Return empty string for invalid dates
+    return ""; // Returns empty string for invalid dates
   }
 
   const year = targetDate.getFullYear();
