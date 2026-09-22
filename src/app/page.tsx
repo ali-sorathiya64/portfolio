6c5f0bfb23ad467053a1c15152eefe45af5a4fa2
Ali sorathiya <alisorathiya970@gmail.com>
2026-09-22 04:12:15 -0700

    replace some stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,8 +40,7 @@ export default function Page() {
                     <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75' />
                     <span className='relative inline-flex size-2 rounded-full bg-green-500' />
                   </span>
-                  <Bug className='size-3' />
-                  probably debugging something right now
+                  Probably debugging something right now
                 </div>
               </BlurFade>
             </div>
